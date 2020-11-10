4b8474c91c14ce451ae2c1cb5764f8e8c379cc8c
daniel-office <danieldng52@gmail.com>
2020-11-10 23:49:44 +0700

    add .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # import db_connect as db
-from lib.utils import Columns
-from lib.utils import Tables
+# from lib.utils import Columns
+# from lib.utils import Tables
 
 if __name__ =='__main__':
     col = Columns('ps_id','UInt8','YES','')
